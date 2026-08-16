c1fc7aa022f55521c50008bccdb9526cded567d2
Wirakorn Thanabat <kurannemaisa@outlook.com>
2026-08-16 17:13:39 +0700

    default.zsh: change from source one script for each dir -> source all script in relate dir

--- .config/zsh/conf.d/env/paths.zsh
+++ .config/zsh/conf.d/env/paths.zsh
@@ -10,12 +10,20 @@ local target_paths=(
   $CARGO_HOME/bin
   $GOPATH/bin
   $HOME/Library/Android/sdk/emulator
-  $BREW_PREFIX/sbin
-  $BREW_PREFIX/opt/openjdk/bin
-  $BREW_PREFIX/opt/llvm/bin
-  $BREW_PREFIX/opt/bison/bin
-  $BREW_PREFIX/bin
 )
+
+if [ -d "$BREW_PREFIX" ]; then
+    local brew_paths=(
+        $BREW_PREFIX/sbin
+        $BREW_PREFIX/opt/openjdk/bin
+        $BREW_PREFIX/opt/llvm/bin
+        $BREW_PREFIX/opt/bison/bin
+        $BREW_PREFIX/bin
+    )
+    for p in ${brew_paths[@]} ; do
+        target_paths+=("$p")
+    done
+fi
 
 local valid_paths=()
 for p in $target_paths; do
