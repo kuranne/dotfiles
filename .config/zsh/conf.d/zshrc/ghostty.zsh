005affd05328e84e7121c3762dfd067578c8afb7
Wirakorn Thanabat <kurannemaisa@outlook.com>
2026-08-31 19:50:44 +0700

    edit: zsh

--- .config/zsh/conf.d/zshrc/ghostty.zsh
+++ .config/zsh/conf.d/zshrc/ghostty.zsh
@@ -1,2 +1,4 @@
 [[ -f "${ZDOTDIR}/conf.d/zshrc/default.zsh" ]] && source "${ZDOTDIR}/conf.d/zshrc/default.zsh"
-source "${ZDOTDIR}/conf.d/zshrc/ghostty"/*.zsh
+for f in "${ZDOTDIR}/conf.d/zshrc/ghostty"/*.zsh; do
+    source "$f"
+done
